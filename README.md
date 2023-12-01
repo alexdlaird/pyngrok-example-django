5c2b9b858600a448863344c1d1b92e62abcb50a0
Alex Laird <contact@alexlaird.com>
2023-12-01 09:00:46 -0600

    Commit Django project with pyngrok example.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pyngrok-example-django
+[![CI/CD](https://github.com/alexdlaird/pyngrok-example-django/workflows/CI/CD/badge.svg)](https://github.com/alexdlaird/pyngrok-example-django/actions?query=workflow%3ACI%2FCD)
+![GitHub License](https://img.shields.io/github/license/alexdlaird/pyngrok-example-django)
+
+# pyngrok Example - Django
+
+This is an example project that shows how to easily integrate [`pyngrok`](https://github.com/alexdlaird/pyngrok)
+with [Django](https://docs.djangoproject.com/en/).
---
+++ pyngrokexampledjango/myapp/apps.py
@@ -0,0 +1,33 @@
+import sys
+from urllib.parse import urlparse
+
+from django.apps import AppConfig
+from django.conf import settings
+
+
+class MyAppConfig(AppConfig):
+    name = "pyngrokexampledjango.myapp"
+    verbose_name = "My App"
+
+    def ready(self):
+        if settings.DEV_SERVER and settings.USE_NGROK:
+            # pyngrok will only be installed, and should only ever be initialized, in a dev environment
+            from pyngrok import ngrok
+
+            # Get the dev server port (defaults to 8000 for Django, can be overridden with the
+            # last arg when calling `runserver`)
+            addrport = urlparse("http://{}".format(sys.argv[-1]))
+            port = addrport.port if addrport.netloc and addrport.port else "8000"
+
+            # Open a ngrok tunnel to the dev server
+            public_url = ngrok.connect(port).public_url
+            print("ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+
+            # Update any base URLs or webhooks to use the public ngrok URL
+            settings.BASE_URL = public_url
+            MyAppConfig.init_webhooks(public_url)
+
+    @staticmethod
+    def init_webhooks(base_url):
+        # Update inbound traffic via APIs to use the public-facing ngrok URL
+        pass
